0541c21528e5c8f7f1fdc9d95e2070d1702ffce4
bmpsst511 <bmpsst511@gmail.com>
2022-11-27 21:05:35 +0800

    effect update

--- Assets/Scripts/FacialControl.cs
+++ Assets/Scripts/FacialControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FacialControl : MonoBehaviour
 {
-    public GameObject still, happy, sad, disgust;
+    public GameObject still, happy, sad, disgust, happyEffect, sadEffect, disgustEffect;
     public bool _stillState, _happyState, _sadState, _disgustState;
     public Animator Tail_Animator;
     // Start is called before the first frame update
@@ -21,7 +21,11 @@ public class FacialControl : MonoBehaviour
             happy.SetActive(false);
             sad.SetActive(false);
             disgust.SetActive(false);
+            happyEffect.SetActive(false);
+            sadEffect.SetActive(false);
+            disgustEffect.SetActive(false);
             Tail_Animator.SetBool("happy", false);
+            Tail_Animator.SetBool("sad", false);
             Tail_Animator.SetBool("disgust", false);
         }
         else if(_happyState){
@@ -29,7 +33,11 @@ public class FacialControl : MonoBehaviour
             happy.SetActive(true);
             sad.SetActive(false);
             disgust.SetActive(false);
+            happyEffect.SetActive(true);
+            sadEffect.SetActive(false);
+            disgustEffect.SetActive(false);
             Tail_Animator.SetBool("happy", true);
+            Tail_Animator.SetBool("sad", false);
             Tail_Animator.SetBool("disgust", false);
         }
         else if(_sadState){
@@ -37,14 +45,37 @@ public class FacialControl : MonoBehaviour
             happy.SetActive(false);
             sad.SetActive(true);
             disgust.SetActive(false);
+            happyEffect.SetActive(false);
+            sadEffect.SetActive(true);
+            disgustEffect.SetActive(false);
+            Tail_Animator.SetBool("happy", false);
+            Tail_Animator.SetBool("sad", true);
+            Tail_Animator.SetBool("disgust", false);
         }
         else if(_disgustState){
             still.SetActive(false);
             happy.SetActive(false);
             sad.SetActive(false);
             disgust.SetActive(true);
+            happyEffect.SetActive(false);
+            sadEffect.SetActive(false);
+            disgustEffect.SetActive(true);
             Tail_Animator.SetBool("happy", false);
+            Tail_Animator.SetBool("sad", false);
             Tail_Animator.SetBool("disgust", true);
+        }
+        else
+        {
+            still.SetActive(true);
+            happy.SetActive(false);
+            sad.SetActive(false);
+            disgust.SetActive(false);
+            happyEffect.SetActive(false);
+            sadEffect.SetActive(false);
+            disgustEffect.SetActive(false);
+            Tail_Animator.SetBool("happy", false);
+            Tail_Animator.SetBool("sad", false);
+            Tail_Animator.SetBool("disgust", false);
         }
     }
 }
